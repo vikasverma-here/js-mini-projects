275d19965e42e319db929d40be22e897717aa8f0
vikasverma-here <vikasverma995619@gmail.com>
2024-11-11 22:05:28 +0530

    some changes on previous project

--- keyboared_voice/script.js
+++ keyboared_voice/script.js
@@ -1,16 +1,38 @@
 
 const body = document.querySelector("body");
-body.addEventListener("keydown", (e) => {
-    const element = document.querySelector(`.${e.key}`);
-    element.style.backgroundColor = "white";
-    element.style.color = "black";
-    element.childNodes[1].play();
-});
+// body.addEventListener("keydown", (e) => {
+//     const element = document.querySelector(`.${e.key}`);
+//     element.style.backgroundColor = "white";
+//     element.style.color = "black";
+//     element.childNodes[1].play();
+// });
 
-body.addEventListener("keyup", (e) => {
-    const element = document.querySelector(`.${e.key}`);
-    element.style.backgroundColor = "black";
-    element.style.color = "white";
+// body.addEventListener("keyup", (e) => {
+//     const element = document.querySelector(`.${e.key}`);
+//     element.style.backgroundColor = "black";
+//     element.style.color = "white";
+//     element.childNodes[1].pause();
+//     element.childNodes[1].load();
+// });
+
+body.addEventListener("keydown",(e)=>{
+    console.log(e)
+    const element = document.querySelector(`.${e.key}`)
+    console.log(element)
+
+    let node =element.childNodes[1].play()
+    console.log(node)
+    element.style.backgroundColor='white'
+    element.style.color='black'
+    
+
+})
+
+body.addEventListener('keyup',(e)=>{
+let element = document.querySelector(`.${e.key}`)
+element.style.backgroundColor="black"
+element.style.color="white"
     // element.childNodes[1].pause();
     // element.childNodes[1].load();
-});
+
+})
