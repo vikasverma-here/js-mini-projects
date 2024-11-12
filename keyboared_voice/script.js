6ad488006ebeb9bdd3da6109f00036d5b0afb62c
vikasverma-here <vikasverma995619@gmail.com>
2024-11-12 23:54:26 +0530

    done

--- keyboared_voice/script.js
+++ keyboared_voice/script.js
@@ -1,19 +1,6 @@
 
 const body = document.querySelector("body");
-// body.addEventListener("keydown", (e) => {
-//     const element = document.querySelector(`.${e.key}`);
-//     element.style.backgroundColor = "white";
-//     element.style.color = "black";
-//     element.childNodes[1].play();
-// });
 
-// body.addEventListener("keyup", (e) => {
-//     const element = document.querySelector(`.${e.key}`);
-//     element.style.backgroundColor = "black";
-//     element.style.color = "white";
-//     element.childNodes[1].pause();
-//     element.childNodes[1].load();
-// });
 
 body.addEventListener("keydown",(e)=>{
     console.log(e)
@@ -32,7 +19,7 @@ body.addEventListener('keyup',(e)=>{
 let element = document.querySelector(`.${e.key}`)
 element.style.backgroundColor="black"
 element.style.color="white"
-    // element.childNodes[1].pause();
+    element.childNodes[1].pause();
     // element.childNodes[1].load();
 
 })
